3ec8d1dc7fea54213ddaf3e88dbe29a15b914942
Honorata92 <h.sajdak1@o2.pl>
2022-06-16 22:52:33 +0200

    Dodaj pliki projektów.

---
+++ Lab7-Entity/Model.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> User { get; set; }
+
+    public string ConnectionString { get; }
+
+    public BloggingContext(string connectionString)
+    {
+        this.ConnectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(this.ConnectionString);
+}
+
+public class Blog
+{
+    public long Id { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new();
+}
+
+public class Task
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Role
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class User
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public long Pesel { get; set; }
+    public string Address { get; set; }
+    public long RoleId { get; set; }
+    public long TaskId { get; set; }
+    public Task Task { get; set; }
+    public Role Role { get; set; }
+}
+
+public class Post
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public long BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
---
+++ Lab7-Entity/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Hint: change `DESKTOP-123ABC\SQLEXPRESS` to your server name
+        //       alternatively use `localhost` or `localhost\SQLEXPRESS`
+
+        string connectionString = @"Data Source=DESKTOP-4E1085R;Initial Catalog=blogdb;Integrated Security=True";
+
+        using (BloggingContext db = new BloggingContext(connectionString))
+        {
+            Console.WriteLine($"Database ConnectionString: {db.ConnectionString}.");
+
+            // Create
+            Console.WriteLine("Inserting a new blog");
+            db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+            db.SaveChanges();
+
+            Console.WriteLine("Inserting a new role");
+            var roleAdmin = db.Add(new Role { Name = "Admin" });
+            var roleReader = db.Add(new Role { Name = "Reader" });
+            db.SaveChanges();
+
+            Console.WriteLine("Inserting a new task");
+            var cl = db.Add(new Task { Name = "Cleaning" });
+            var sl = db.Add(new Task { Name = "Slepping" });
+            var lea = db.Add(new Task { Name = "Learning" });
+            db.SaveChanges();
+
+            Console.WriteLine("Inserting a new user");
+            db.Add(new User { Name = "Adam Nowak", Pesel = 95874456881, Address = "Stalowa 8", RoleId = roleAdmin.Entity.Id, TaskId = cl.Entity.Id });
+            db.Add(new User { Name = "Zofia Nowak", Pesel = 99974456881, Address = "Stalowa 8", RoleId = roleReader.Entity.Id, TaskId = sl.Entity.Id });
+            var userUpdate = db.Add(new User { Name = "Zofia Nowak", Pesel = 99974456881, Address = "Stalowa 8", RoleId = roleReader.Entity.Id, TaskId = sl.Entity.Id });
+            db.SaveChanges();
+
+            // Read
+            Console.WriteLine("Querying for a blog");
+
+            Blog blog = db.Blogs
+                .OrderBy(b => b.Id)
+                .First();
+            User user = db.User
+                .OrderBy(b => b.Address)
+                .Last();
+
+            int count = db.Roles
+                .Count();
+
+            int task = db.Tasks
+                .Where(u => u.Name.StartsWith("S"))
+                .Count();
+
+            // Update
+            Console.WriteLine("Updating the blog and adding a post");
+
+            blog.Url = "https://devblogs.microsoft.com/dotnet";
+            blog.Posts.Add(new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+            user.Name = "Marek";
+            userUpdate.Entity.Name = "Elżbieta";
+            db.SaveChanges();
+
+            // Delete
+            Console.WriteLine("Delete the blog");
+
+            db.Remove(blog);
+            db.Remove(user);
+            // db.Remove(task);
+            db.SaveChanges();
+        }
+    }
+}
